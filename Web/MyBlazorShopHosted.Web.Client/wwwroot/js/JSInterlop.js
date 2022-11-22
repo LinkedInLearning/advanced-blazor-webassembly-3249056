be445884c16d60f7aa3a77fccbc920cd17e62937
David Grace <davidgrace00@gmail.com>
2022-11-22 19:55:49 +0000

    Added JavaScript file

---
+++ Web/MyBlazorShopHosted.Web.Client/wwwroot/js/JSInterlop.js
@@ -0,0 +1,8 @@
+﻿function AddToCart(productName) {
+    alert(productName + " has been added to the cart");
+}
+function GetLocalOffsetMinutes() {
+    var date = new Date();
+
+    return date.getTimezoneOffset();
+}
