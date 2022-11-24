28671a87db88d3fd0fcb73adc89791e478946a3d
David Grace <davidgrace00@gmail.com>
2022-11-24 19:05:15 +0000

    Added JS functions

--- Web/MyBlazorShopHosted.Web.Client/wwwroot/js/JSInterlop.js
+++ Web/MyBlazorShopHosted.Web.Client/wwwroot/js/JSInterlop.js
@@ -1,8 +1,8 @@
-﻿function AddToCart(productName) {
-    alert(productName + " has been added to the cart");
-}
-function GetLocalOffsetMinutes() {
+﻿function GetLocalOffsetMinutes() {
     var date = new Date();
 
     return date.getTimezoneOffset();
+}
+function AddToCart(productName) {
+    alert(productName + " has been added to the cart");
 }
