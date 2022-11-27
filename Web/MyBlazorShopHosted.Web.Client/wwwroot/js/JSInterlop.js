ffb429be570c9f509f3fd4594a4d2a625f605982
David Grace <davidgrace00@gmail.com>
2022-11-27 13:53:44 +0000

    Added modules

--- Web/MyBlazorShopHosted.Web.Client/wwwroot/js/JSInterlop.js
+++ Web/MyBlazorShopHosted.Web.Client/wwwroot/js/JSInterlop.js
@@ -1,8 +1,8 @@
-﻿function GetLocalOffsetMinutes() {
+﻿export function GetLocalOffsetMinutes() {
     var date = new Date();
 
     return date.getTimezoneOffset();
 }
-function AddToCart(productName) {
+export function AddToCart(productName) {
     alert(productName + " has been added to the cart");
 }
