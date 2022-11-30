7492378b4831304bac658d65ed8d1995e81ab781
David Grace <davidgrace00@gmail.com>
2022-11-30 19:36:59 +0000

    Added details

--- Web/MyBlazorShopHosted.Web.Client/wwwroot/js/JSInterlop.js
+++ Web/MyBlazorShopHosted.Web.Client/wwwroot/js/JSInterlop.js
@@ -1,3 +1,11 @@
-﻿export function AddToCart(productName) {
-    alert(productName + " has been added to the cart");
+﻿export function ToggleElement(element) {
+    if (element == undefined) {
+        return;
+    }
+    if (element.style.display == "none") {
+        element.style.display = "block";
+    }
+    else {
+        element.style.display = "none";
+    }
 }
