babbc35784776c86cfbd34a3e69e6f2fa15b2083
David Grace <davidgrace00@gmail.com>
2022-12-02 15:01:56 +0000

    Added streaming images

---
+++ Web/MyBlazorShopHosted.Web.Client/wwwroot/js/StreamImage.js
@@ -0,0 +1,10 @@
+﻿export function LoadImageThroughStream(imageElement, imageStream) {
+    const arrayBuffer = imageStream.arrayBuffer();
+    const blob = new Blob([arrayBuffer]);
+    const url = URL.createObjectURL(blob);
+
+    imageElement.onload = () => {
+        URL.revokeObjectURL(url);
+    }
+    imageElement.src = url;
+}
