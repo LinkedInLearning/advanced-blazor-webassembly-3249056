00da950b8c5f7fec6a6fba28f395c88def12080a
David Grace <davidgrace00@gmail.com>
2022-12-08 19:25:43 +0000

    Added connection and message.

--- Web/MyBlazorShopHosted.Web.Server/Hubs/LiveChatHub.cs
+++ Web/MyBlazorShopHosted.Web.Server/Hubs/LiveChatHub.cs
@@ -4,5 +4,9 @@ namespace MyBlazorShopHosted.Web.Server.Hubs
 {
     public class LiveChatHub : Hub
     {
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.Client(Context.ConnectionId).SendAsync("ReceiveMessage", "Welcome. Please enter your message.");
+        }
     }
 }
