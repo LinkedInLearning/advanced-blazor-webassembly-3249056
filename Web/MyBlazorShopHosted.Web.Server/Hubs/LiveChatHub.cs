5c39c70f5d1635fef51ac9ce8666a4f88091b020
David Grace <davidgrace00@gmail.com>
2022-12-11 14:47:52 +0000

    Added send message

--- Web/MyBlazorShopHosted.Web.Server/Hubs/LiveChatHub.cs
+++ Web/MyBlazorShopHosted.Web.Server/Hubs/LiveChatHub.cs
@@ -8,5 +8,15 @@ namespace MyBlazorShopHosted.Web.Server.Hubs
         {
             await Clients.Client(Context.ConnectionId).SendAsync("ReceiveMessage", "Welcome. Please enter your message.");
         }
+
+        public async Task SendMessageAsync(string message)
+        {
+            await Task.Delay(new TimeSpan(0, 0, 3));
+
+            if (message.Contains("delivery", StringComparison.OrdinalIgnoreCase))
+            {
+                await Clients.Client(Context.ConnectionId).SendAsync("ReceiveMessage", "Delivery will take 5-7 days depending on location.");
+            }
+        }
     }
 }
