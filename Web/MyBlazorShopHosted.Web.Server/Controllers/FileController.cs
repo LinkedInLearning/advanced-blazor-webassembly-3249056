2653d534e8315f169998e3aab02dd7a57f95211a
David Grace <davidgrace00@gmail.com>
2022-12-12 19:27:54 +0000

    Added file upload

---
+++ Web/MyBlazorShopHosted.Web.Server/Controllers/FileController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MyBlazorShopHosted.Web.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileController : ControllerBase
+    {
+        private readonly IWebHostEnvironment _environment;
+
+        public FileController(IWebHostEnvironment environment)
+        {
+            _environment = environment;
+        }
+
+        /// <summary>
+        /// Uploads a file up to the server
+        /// </summary>
+        /// <param name="file">The file</param>
+        /// <returns>A response about the upload</returns>
+        public async Task<ActionResult> UploadAsync(IFormFile file)
+        {
+            // Get a random file name
+            var fileName = Path.GetRandomFileName();
+
+            // Populate into uploads folder
+            var folderPath = Path.Combine(_environment.ContentRootPath, "uploads");
+
+            if (!Directory.Exists(folderPath))
+            {
+                // Create directory if it does not exist.
+                Directory.CreateDirectory(folderPath);
+            }
+
+            // Get full file path
+            var path = Path.Combine(folderPath, fileName);
+
+            // Upload file
+            await using FileStream fs = new(path, FileMode.Create);
+            await file.CopyToAsync(fs);
+
+            return Ok(new { Filename = fileName });
+        }
+    }
+}
