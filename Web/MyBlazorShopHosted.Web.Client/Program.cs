442af6aa36765d47335ac77826da2c2b3960539e
David Grace <davidgrace00@gmail.com>
2022-12-30 14:36:40 +0000

    Remove content from last video

--- Web/MyBlazorShopHosted.Web.Client/Program.cs
+++ Web/MyBlazorShopHosted.Web.Client/Program.cs
@@ -1,10 +1,7 @@
-using Blazored.LocalStorage;
-using Blazored.SessionStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MyBlazorShopHosted.Web.Client;
 using MyBlazorShopHosted.Web.Client.StateManagement;
-using System.Globalization;
 using System.Net.Http.Headers;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -20,27 +17,6 @@ http.DefaultRequestHeaders.CacheControl = new CacheControlHeaderValue
     NoCache = true
 };
 builder.Services.AddScoped(sp => http);
-
-using var response = await http.GetAsync("productlisting.json");
-using var stream = await response.Content.ReadAsStreamAsync();
-
-builder.Configuration.AddJsonStream(stream);
-
-using var environmentResponse = await http.GetAsync("productlisting." + builder.HostEnvironment.Environment + ".json");
-
-if (environmentResponse.IsSuccessStatusCode)
-{
-    using var environmentStream = await environmentResponse.Content.ReadAsStreamAsync();
-    builder.Configuration.AddJsonStream(environmentStream);
-}
-
-builder.Services.AddLocalization();
-
-CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-US");
-CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("en-US");
-
-builder.Services.AddBlazoredLocalStorage();
-builder.Services.AddBlazoredSessionStorage();
 builder.Services.AddSingleton<IShoppingCartStateContainer, ShoppingCartStateContainer>();
 
 await builder.Build().RunAsync();
