704ab5936c7c620a0ffb916b64d593e6766c6fcc
David Grace <davidgrace00@gmail.com>
2023-01-07 11:57:49 +0000

    Initial project

---
+++ Testing/MyBlazorShopHosted.Testing.E2E.Client/BaseTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Playwright;
+using Microsoft.Playwright.MSTest;
+
+namespace MyBlazorShopHosted.Testing.E2E.Client
+{
+    public abstract class BaseTest
+    {
+
+
+        protected async Task CreateBrowser()
+        {
+            var playwright = await Playwright.CreateAsync();
+
+            
+            await using var browser = await Playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+            {
+                Headless = false
+            });
+
+            var page = await browser.NewPageAsync();
+            
+        }
+    }
+}
---
+++ Testing/MyBlazorShopHosted.Testing.E2E.Client/ProductToCartTests.cs
@@ -0,0 +1,16 @@
+namespace MyBlazorShopHosted.Testing.E2E.Client
+{
+    public class ProductToCartTests
+    {
+        public ProductToCartTests()
+        {
+
+        }
+
+        [Fact]
+        public void Page_AddsProductToCart_ShowsCartPage()
+        {
+
+        }
+    }
+}
