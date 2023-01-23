f08515fe23a6fc8cd14f113589a391cc04997af7
David Grace <davidgrace00@gmail.com>
2023-01-23 14:16:58 +0000

    Added code from video

--- Web/MyBlazorShopHosted.Web.Server/Hubs/LiveChatHub.cs
+++ Web/MyBlazorShopHosted.Web.Server/Hubs/LiveChatHub.cs
@@ -6,7 +6,19 @@ namespace MyBlazorShopHosted.Web.Server.Hubs
     {
         public override async Task OnConnectedAsync()
         {
-            await Clients.Client(Context.ConnectionId).SendAsync("ReceiveMessage", "Welcome. Please enter your message.");
+            await Clients.Client(Context.ConnectionId)
+                .SendAsync("ReceiveMessage", "Welcome. Please enter your message.");
+        }
+
+        public async Task SendMessageAsync(string message)
+        {
+            await Task.Delay(new TimeSpan(0, 0, 3));
+
+            if (message.Contains("delivery", StringComparison.OrdinalIgnoreCase))
+            {
+                await Clients.Client(Context.ConnectionId)
+                    .SendAsync("ReceiveMessage", "Delivery will take 5-7 days depending on location.");
+            }
         }
     }
 }
