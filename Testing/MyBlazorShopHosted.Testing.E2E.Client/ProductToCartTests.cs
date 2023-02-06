9c2de061980597c7e15ed089333b58ced1c209e3
David Grace <davidgrace00@gmail.com>
2023-02-06 10:57:24 +0000

    Code changes from video

--- Testing/MyBlazorShopHosted.Testing.E2E.Client/ProductToCartTests.cs
+++ Testing/MyBlazorShopHosted.Testing.E2E.Client/ProductToCartTests.cs
@@ -6,6 +6,21 @@ namespace MyBlazorShopHosted.Testing.E2E.Client
         public async void Page_AddsProductToCart_ShowsCartPage()
         {
            var page = await CreateBrowserAsync();
+            await page.GotoAsync("https://localhost:9002");
+
+            await Task.Delay(TimeSpan.FromSeconds(5));
+
+            await page.Locator("a[data-test='BLADE-MUG']").ClickAsync();
+            await page.WaitForURLAsync("https://localhost:9002/product/blade-mug");
+
+            await Task.Delay(TimeSpan.FromSeconds(5));
+
+            await page.Locator("button[data-test='add-to-cart']").ClickAsync();
+            await page.WaitForURLAsync("https://localhost:9002/cart");
+
+            await Task.Delay(TimeSpan.FromSeconds(5));
+
+            Assert.True(page.Url == "https://localhost:9002/cart");
         }
     }
 }
